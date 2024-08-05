520bb8aea473749a00f922f5e5cf3a2104c42740
ed006949 <1711185+ed006949@users.noreply.github.com>
2024-08-06 02:32:19 +0300

    Makefile commit (Tue Aug  6 02:32:19 MSK 2024)

--- src/l/var.go
+++ src/l/var.go
@@ -23,20 +23,3 @@ var (
 		Daemon: "daemon",
 	}
 )
-
-// var (
-// 	verbosityDescription = map[verbosityLevel]string{
-// 		// Emergency:     zerolog.Level(Emergency).String(),     // zerolog.FatalLevel
-// 		// Alert:         zerolog.Level(Alert).String(),         // zerolog.FatalLevel
-// 		Critical: zerolog.Level(Critical).String(), // zerolog.FatalLevel
-// 		Error:    zerolog.Level(Error).String(),    // zerolog.ErrorLevel
-// 		Warning:  zerolog.Level(Warning).String(),  // zerolog.WarnLevel
-// 		// Notice:        zerolog.Level(Notice).String(),        // zerolog.InfoLevel
-// 		Informational: zerolog.Level(Informational).String(), // zerolog.InfoLevel
-// 		Debug:         zerolog.Level(Debug).String(),         // zerolog.DebugLevel
-// 		Trace:         zerolog.Level(Trace).String(),         // zerolog.TraceLevel
-// 		Panic:         zerolog.Level(Panic).String(),         // zerolog.PanicLevel
-// 		Quiet:         zerolog.Level(Quiet).String(),         // zerolog.NoLevel
-// 		Disabled:      zerolog.Level(Disabled).String(),      // zerolog.Disabled
-// 	}
-// )
